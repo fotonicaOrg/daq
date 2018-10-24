4db649919532fe17b4dc91f0d6ddda26c5ff1f07
Axel Lacapmesure <axel.lacapmesure@gmail.com>
2018-10-24 19:26:52 -0300

    Clase del 24/Oct
    Se actualiza el script para incorporar manejo de counters y hacer PWM.

--- main_temperatura.py
+++ main_temperatura.py
@@ -13,33 +13,58 @@ else:
     importlib.reload(daq)
 
 import nidaqmx
+import nidaqmx.stream_writers
 import numpy as np
 from matplotlib import pyplot as plt
+import time
 
 CAL = 100
 
-channels = ('Dev2/ai0')
-voltage_range = ([-10,10])
-n_samples = 10
-freq = 100
+ai_channels = ('Dev1/ai0')
+co_channels = ('Dev1/ctr0')
+pwm_freq = 100
+pwm_duty_cycle = 0.5
 
-with nidaqmx.Task() as task:        
+voltage_range = ([-10,10])
+n_samples = 100
+freq = 100000
+
+with nidaqmx.Task() as task_ai, nidaqmx.Task() as task_co:
+    
+    daq.configure_ai(
+            task_ai,
+            physical_channels = ai_channels,
+            voltage_range = voltage_range,
+            terminal_configuration = nidaqmx.constants.TerminalConfiguration.RSE
+            )
+    
+    chan_co = daq.configure_pwm(
+            task_co,
+            physical_channels = co_channels,
+            frequency = pwm_freq,
+            duty_cycle = pwm_duty_cycle
+            )
+    
+    task_co.timing.cfg_implicit_timing(sample_mode = nidaqmx.constants.AcquisitionType.CONTINUOUS)
+    
+    stream_co = nidaqmx.stream_writers.CounterWriter(task_co.out_stream)
+    task_co.start()
+    
     (data, real_freq) = daq.continuous_acquire(
-                task,
-                n_samples,
-                channels,
-                freq,
-                voltage_range = voltage_range,
-                acquisition_units = nidaqmx.constants.VoltageUnits.VOLTS,
-                terminal_configuration = nidaqmx.constants.TerminalConfiguration.RSE
+                task = task_ai,
+                n_samples = n_samples,
+                sample_frequency = freq,
+                task_co = task_co,
+                stream_co = stream_co,
+                chan_co = chan_co[0]
                 )
 
 time = np.arange(data.size) / real_freq
-
-plt.plot(time, data[0,:])
-plt.xlabel('Tiempo (s)')
-plt.ylabel('Tensión registrada (V)')
-plt.grid()
+#
+#plt.plot(time, data[0,:])
+#plt.xlabel('Tiempo (s)')
+#plt.ylabel('Tensión registrada (V)')
+#plt.grid()
 
 
 
